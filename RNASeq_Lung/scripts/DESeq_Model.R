143187a482cd5bc8883a6c4c392d4aa71ca416df
André Barros <andrebolerbarros@gmail.com>
2022-04-27 21:42:59 +0100

    Update DESeq_Model.R

--- RNASeq_Lung/scripts/DESeq_Model.R
+++ RNASeq_Lung/scripts/DESeq_Model.R
@@ -22,35 +22,8 @@ setwd("~/Colaco_etal_2019/RNASeq_Lung/")
 dir.create(path = "GeneralPlots/", showWarnings = FALSE)
 dir.create(path = "SessionInfo/", showWarnings = FALSE)
 
-#Save a file with the information of the sessionInfo
-writeLines(capture.output(sessionInfo()),paste0("SessionInfo/SessionInfo_",Sys.Date(),".txt",sep=""))
-
-#The ReadsPerGene files are saved in a folder called gene_counts. We need to save a list of files to import
-gc_tab<-"gene_counts/"
-file.names<-paste0(gc_tab,dir(gc_tab,pattern="tab"),sep="")
-
-#Importing each file as data-frame and save them in a list
-outfile<-list()
-for (i in file.names) {
-  outfile[[i]]<-read.table(i,row.names=1,comment.char="")
-}
-
-#Remove the first for lines, since they will not be used in this analysis
-for (i in 1:length(outfile)) {
-  outfile[[i]]<-outfile[[i]][-c(1:4),]
-}
-
-#Now, we have a data-frame for each sample. The column to be considered for each sample is based on the strandness of the sequencing protocol
-tab<-data.frame(outfile[[1]][,3])
-rownames(tab)<-rownames(outfile[[1]])
-
-for (i in 2:length(outfile)) {
-  tab[,i]<-outfile[[i]][,3]
-  rownames(tab)<-rownames(outfile[[1]])
-}
-
-#Re-name the columns to get only the sample names and remove the path of each file & extension
-colnames(tab)<-gsub("^.*(lg[5-8][a-f]+).*","\\1",names(outfile))
+#Import the all_counts file
+tab<-read.csv("gene_counts/all_counts.tab",row.names = 1)
 
 #Import the metadata file & add a new variable that combines treatment & infection
 design<-read.csv2("sampleTable.csv",header=T,row.names = 1,sep=";")
